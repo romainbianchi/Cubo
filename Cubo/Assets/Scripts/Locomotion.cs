3d46a4f9261d5a98c74e3dba322a3a82c74cc12d
romainbianchi <104493944+romainbianchi@users.noreply.github.com>
2023-05-17 16:30:47 +0200

    Tp + distGrab ok

---
+++ Cubo/Assets/Scripts/Locomotion.cs
@@ -0,0 +1,255 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Locomotion : MonoBehaviour
+{
+
+	// Select the controller type
+    public enum ControllerType {LeftController, RightController}
+    public ControllerType controllerType = ControllerType.LeftController;
+
+
+    [Header( "Maximum Distance" )]
+	[Range( 2f, 30f )]
+	// Store the maximum distance the player can teleport
+	public float maximumTeleportationDistance = 15f;
+
+
+    [Header( "Marker" )]
+	// Store the refence to the marker prefab used to highlight the targeted point
+	public GameObject markerPrefab;
+	protected GameObject marker_prefab_instanciated;
+
+
+    // Player
+    [Header("Player")]
+    public OVRPlayerController player;
+    public PlayerControllerPers playerPers;
+    private CharacterController character_controller;
+
+
+    //Point de contact avec le sol
+    public RaycastHit hit;
+
+    //Raycast parameters
+    private List<Vector3> positions;
+    private LineRenderer lineRenderer;
+    private Vector3 raycastDirection;
+    private Vector3 raycastPosition;
+
+   
+    //Settings
+    public int rayCastSpawnLimit = 200;
+    public float rayCastLength = 0.1f;
+    public float gravity = 7.5f;
+    public float smooth = 0.01f;
+    private int amountOfRaycastsSpawned;
+    public float rayCastEnlargorFactor = 2.0f; // facteur pour regler les pb de collisions
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         character_controller = player.GetComponent<CharacterController>();
+
+        lineRenderer = GetComponent<LineRenderer>(); 
+    }
+
+
+    // Keep track of the teleportation state to prevent continuous teleportation
+	protected bool teleportation_locked = false;
+
+    // Keep track of the pointing state
+	// protected bool left_pointing = false;
+    // protected bool right_pointing = false;
+
+    // Store Tp target point
+    protected Vector3 target_point;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // State check
+        if (controllerType == ControllerType.LeftController && (playerPers.getLeftState() == PlayerControllerPers.State.Grabbing || playerPers.getLeftState() == PlayerControllerPers.State.DistanceGrabbing)) return;
+        if (controllerType == ControllerType.RightController && (playerPers.getRightState() == PlayerControllerPers.State.Grabbing || playerPers.getRightState() == PlayerControllerPers.State.DistanceGrabbing)) return;
+        // Handle locomotion behavior
+        handle_locomotion();
+    }
+
+    // Handle locomotion
+    public void handle_locomotion()
+    {
+
+        // Left controller Tp
+        if (controllerType == ControllerType.LeftController)
+        {
+            // If the player is pointing
+            if (OVRInput.Get(OVRInput.Button.Three) && !playerPers.getRightTpPointing())
+            {
+
+                // Left controller occupied
+                playerPers.setLeftState(PlayerControllerPers.State.Locomotion);
+
+                // Left controller pointing state
+                playerPers.setLeftTpPointing(true);
+
+                // Vector3 valid target_point
+                if (aim_with(out target_point))
+                {
+
+                    // Instantiate the marker prefab if it doesn't already exists and place it to the targeted position
+                    if ( marker_prefab_instanciated == null ) marker_prefab_instanciated = GameObject.Instantiate( markerPrefab, this.transform );
+                    marker_prefab_instanciated.transform.position = target_point;
+
+                    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+                    {
+                        // Tp the player
+                        character_controller.Move(target_point - this.transform.position);
+                    }
+
+                }
+
+            } else {
+
+                // Left controller available
+                playerPers.setLeftState(PlayerControllerPers.State.Idle);
+
+                // Left controller not pointing
+                playerPers.setLeftTpPointing(false);
+
+                // destroy the marker prefab if it exists
+                if ( marker_prefab_instanciated != null ) Destroy( marker_prefab_instanciated);
+                marker_prefab_instanciated = null;
+
+                lineRenderer.enabled = false;
+                lineRenderer.startColor = Color.red;
+
+            }
+        }
+
+
+        // Right controller Tp
+        if (controllerType == ControllerType.RightController)
+        {
+            
+            // If the player is pointing
+            if (OVRInput.Get(OVRInput.Button.One) && !playerPers.getLeftTpPointing())
+            {
+
+                // Right controller occupied
+                playerPers.setRightState(PlayerControllerPers.State.Locomotion);
+
+                // Right controller pointing state
+                playerPers.setRightTpPointing(true);
+
+                // Vector3 valid target_point
+                if (aim_with(out target_point))
+                {
+
+                    // Instantiate the marker prefab if it doesn't already exists and place it to the targeted position
+                    if ( marker_prefab_instanciated == null ) marker_prefab_instanciated = GameObject.Instantiate( markerPrefab, this.transform );
+                    marker_prefab_instanciated.transform.position = target_point;
+
+                    if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+                    {
+                        // Tp the player
+                        character_controller.Move(target_point - this.transform.position);
+                    }
+
+                } 
+
+            } else {
+
+                // Right controller available
+                playerPers.setRightState(PlayerControllerPers.State.Idle);
+
+                // Right controller not pointing
+                playerPers.setRightTpPointing(false);
+
+                // destroy the marker prefab if it exists
+                if ( marker_prefab_instanciated != null ) Destroy( marker_prefab_instanciated);
+                marker_prefab_instanciated = null;
+                lineRenderer.enabled = false;
+                lineRenderer.startColor = Color.red;
+
+            }
+        }
+
+    }
+
+
+	protected bool aim_with (out Vector3 target_point) {
+
+		// Default the "output" target point to the null vector
+		target_point = new Vector3();
+
+		// Launch the ray cast and leave if it doesn't hit anything
+		RaycastHit hit;
+		hit = getHit();
+
+		// If the aimed point is out of range (i.e. the raycast distance is above the maximum distance) then prevent the teleportation
+		if ( hit.distance > maximumTeleportationDistance ) return false;
+
+		// "Output" the target point
+		target_point = hit.point;
+		return true;
+	}
+
+    private RaycastHit getHit()
+    {
+        // Ici on obtient la liste de points de la parabole (et on lance le raycast a l'intérieur de la fonction)
+        Vector3[] curvePoints = curvedRaycast();
+
+        // Ici on update les parametre du line renderer en fonction de la liste de points
+        int numPoints = curvePoints.Length;
+        lineRenderer.positionCount = numPoints;
+
+        // Ici on update les positions du line renderer en fonction de la liste de points
+        for (int i = 0; i < numPoints; i++)
+        {
+            Vector3 point = curvePoints[i];
+            lineRenderer.SetPosition(i, point);
+        }
+
+        lineRenderer.startColor = Color.green;
+        lineRenderer.enabled = true;
+
+        return hit;
+
+    }
+
+
+    private Vector3[] curvedRaycast()
+    {
+        amountOfRaycastsSpawned = 0;
+        positions = new List<Vector3>();
+        raycastPosition = transform.position;
+        raycastDirection = transform.forward;
+        
+        // Tant que le nombre de raycast est inferieur au nombre de raycast max on continue a creer la parabole
+        while (amountOfRaycastsSpawned < rayCastSpawnLimit)
+        {
+            positions.Add(raycastPosition);
+
+            // Si le raycast touche un objet, on arrete le raycast
+            if (Physics.Raycast(raycastPosition, raycastDirection, out hit, rayCastEnlargorFactor*rayCastLength))
+            {
+                positions.Add(hit.point);
+                break;
+            }
+            
+            // Sinon on append le raycast
+            raycastPosition += raycastDirection * rayCastLength;
+            raycastDirection += new Vector3(0.0f, -gravity * smooth, 0.0f);
+            
+            amountOfRaycastsSpawned++;
+        }
+        
+        // On retourne la liste de points pour afficher le line renderer apres l'appel de la fonction
+        return positions.ToArray();
+    }
+
+}
---
+++ Cubo/Assets/Scripts/PlayerControllerPers.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerPers : MonoBehaviour
+{
+    // Refer to a position of in_cubo
+    public GameObject inCuboPos;
+
+    // States
+    public enum State {Locomotion, DistanceGrabbing, Grabbing, Idle}
+    private State right_state = State.Idle;
+    private State left_state = State.Idle;
+
+    // Controllers pointing states
+    private bool left_tp_pointing = false;
+    private bool right_tp_pointing = false;
+
+    private static bool inCubo = false;
+    private static Vector3 deskPos;
+
+    public void TpPlayer()
+    {
+        // if the player is in the cube, teleport them to the center
+        if (!inCubo)
+        {
+            transform.position = inCuboPos.transform.position;
+            inCubo = true; 
+            
+        } else {
+            inCuboPos.transform.position = transform.position;
+            transform.position = deskPos;
+            inCubo = false;
+        }
+    }
+
+    // Set and get Tp pointing states
+    public void setLeftTpPointing(bool state)
+    {
+        left_tp_pointing = state;
+    }
+
+    public void setRightTpPointing(bool state)
+    {
+        right_tp_pointing = state;
+    }
+
+    public bool getLeftTpPointing()
+    {
+        return left_tp_pointing;
+    }
+
+    public bool getRightTpPointing()
+    {
+        return right_tp_pointing;
+    }
+
+    // Set and get states
+    public void setRightState(State state)
+    {
+        right_state = state;
+    }
+
+    public State getRightState()
+    {
+        return right_state;
+    }
+
+    public void setLeftState(State state)
+    {
+        left_state = state;
+    }
+
+    public State getLeftState()
+    {
+        return left_state;
+    }
+
+
+	void Start () 
+    { 
+        // Set the initial position of the desk pos
+        deskPos = transform.position;
+
+        // Set the initial position of the in_cubo_pos
+        inCuboPos.transform.position = new Vector3(inCuboPos.transform.position.x, transform.position.y, inCuboPos.transform.position.z);
+    }
+
+    void Update()
+    {
+        
+    } 
+}
