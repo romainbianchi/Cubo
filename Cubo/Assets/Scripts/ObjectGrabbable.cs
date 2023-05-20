589ae3a4023678c7b883bab4fdf57e09c4f25585
Ludo <pujolludo@gmail.com>
2023-05-21 00:16:29 +0200

    Fill Container (cup fonctionnelle avec des glacons) + Commencé à ajouter les objets importants

--- Cubo/Assets/Scripts/ObjectGrabbable.cs
+++ Cubo/Assets/Scripts/ObjectGrabbable.cs
@@ -6,6 +6,13 @@ using static OVRSkeleton;
 
 public class ObjectGrabbable : MonoBehaviour
 {
+    // List of imortant objects
+    public enum ImportantObject { None, Stick, Torch, Cup, Key, CuboToBeContinued };
+    public ImportantObject importantObject = ImportantObject.None;
+
+    // If important object is not none, we have to specify the respawn position
+    public Transform respawnPosition;
+
     // First set the grabbing radius of the anchor
     public float grabbingRadius = 0.1f;
 
@@ -27,8 +34,17 @@ public class ObjectGrabbable : MonoBehaviour
         available = true;
         attachedGrabber = null;
 
-        // Set the layer as "Grabbable"
+        // Set the layer as "Grabbable" (and the children)
         gameObject.layer = LayerMask.NameToLayer("Grabbable");
+        foreach (Transform child in transform){
+            child.gameObject.layer = LayerMask.NameToLayer("Grabbable");
+        }
+
+        // Same for the tag
+        gameObject.tag = "Grabbable";
+        foreach (Transform child in transform){
+            child.gameObject.tag = "Grabbable";
+        }
     }
 
     // When the object is grasped: set the anchor as the child of the hand
@@ -69,22 +85,38 @@ public class ObjectGrabbable : MonoBehaviour
         // Set the parent of the anchor to the initial parent
         transform.SetParent(initialParent);
         
-        if (grabbedBy.controllerType == Grabber.ControllerType.LeftController)
-        {
-            GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch);
-            GetComponent<Rigidbody>().angularVelocity = -OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.LTouch);
+        // If the object is dropped in the desk scene
+        if (transform.position.x < 3.5f) {
+            
+            // If not important object, then drop it normally
+            if (importantObject == ImportantObject.None) {
+                if (grabbedBy.controllerType == Grabber.ControllerType.LeftController)
+                {
+                    GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch);
+                    GetComponent<Rigidbody>().angularVelocity = -OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.LTouch);
+                }
+                else if (grabbedBy.controllerType == Grabber.ControllerType.RightController)
+                {
+                    GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
+                    GetComponent<Rigidbody>().angularVelocity = -OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.RTouch);
+                }
+            }
+            // If important object, then drop it in the right place
+            else {
+                transform.position = respawnPosition.position;
+                transform.rotation = respawnPosition.rotation;
+            }
+        } else {
+            if (grabbedBy.controllerType == Grabber.ControllerType.LeftController)
+            {
+                GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch);
+                GetComponent<Rigidbody>().angularVelocity = -OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.LTouch);
+            }
+            else if (grabbedBy.controllerType == Grabber.ControllerType.RightController)
+            {
+                GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
+                GetComponent<Rigidbody>().angularVelocity = -OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.RTouch);
+            }
         }
-        else if (grabbedBy.controllerType == Grabber.ControllerType.RightController)
-        {
-            GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
-            GetComponent<Rigidbody>().angularVelocity = -OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.RTouch);
-        }
-        // Because thee Oculus plugin does not provide informations about
-        // the speed of the hands when they are not controllers, we cannot 
-        // make the object follow the trajectory if released by a hand.
-        // However, we could get the position of the idextip, at two different 
-        // frames, and compute the speed of the hand by deviding the distance
-        // between the two positions by the time elapsed between the two frames 
-        // (= compputig the speed ourselves).
     }
 }
